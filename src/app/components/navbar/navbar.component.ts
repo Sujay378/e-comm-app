80542a6f79a5eaad443be5a2c31ff890435a6918
Indrayudh <indrayudhricku@gmail.com>
2023-08-12 19:25:59 +0530

    Made changes for the navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  isLoggedIn: boolean = false;
+  username: string = '';
+  password: string = '';
 
+  login() {
+    if (this.username === 'user' && this.password === 'pass') {
+      this.isLoggedIn = true;
+    }
+  }
+
+  logout() {
+    this.isLoggedIn = false;
+    this.username = '';
+    this.password = '';
+  }
 }
